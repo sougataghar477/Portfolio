b5781055ad612ad2a744ec0cd2de8dc9b5d43f4c
sougata <sougataghar47@gmail.com>
2024-03-14 01:31:25 +0530

    added navigation for mobile

--- src/App.js
+++ src/App.js
@@ -3,8 +3,20 @@ import {
   Container,
   Flex,
   Box, 
-  Button,Heading,useColorMode } from '@chakra-ui/react';
-import { SunIcon,MoonIcon } from '@chakra-ui/icons';
+  Button,
+  Heading,
+  ButtonGroup,
+  Drawer,
+  DrawerBody,
+  DrawerHeader,
+  DrawerContent,
+  DrawerCloseButton,
+  useColorMode,
+  useDisclosure,
+  
+} from '@chakra-ui/react';
+  
+import { SunIcon,MoonIcon,HamburgerIcon } from '@chakra-ui/icons';
   import { BrowserRouter as Router, Routes, Route, NavLink   } from 'react-router-dom';
 import Home from './components/Home';
 import About from './components/About';
@@ -13,24 +25,51 @@ import Contact from './components/Contact';
 const links=[{name:'🏡  Home',to:'/'},{name:'📋  Projects',to:'/projects'},{name:'📞  Contact',to:'/contact'}]
 function App() {
   const { colorMode, toggleColorMode } = useColorMode();
+  const { isOpen, onOpen, onClose } = useDisclosure()
   return (
 
       <Router>
         <Container maxW='90ch'>
       <Flex justifyContent={'space-between'} alignItems={'center'}>
         <Heading>Sougata Ghar</Heading>
-        <Button onClick={toggleColorMode} 
+        <ButtonGroup>
+        <Button 
+        onClick={toggleColorMode} 
         mt={4}  
          >{colorMode==='light'?<><SunIcon  />  </>:<><MoonIcon  />  </>}</Button>
+        <Button display={['flex','flex','none']} onClick={onOpen} mt={4} ><HamburgerIcon/></Button>
+        <Drawer
+        isOpen={isOpen}
+        placement='right'
+        onClose={onClose} 
+      >
+        <DrawerContent>
+          <DrawerCloseButton />
+           
+          <DrawerBody mt={8}>
+          {links.map(link=> 
+            <NavLink 
+            onClick={onClose}
+            className={`link ${(isActive)=>isActive ? "active" : null}`}  
+            to={link.to}>
+              <li style={{listStyleType:'none',fontWeight:'bold',width:'250px'}}>{link.name}</li>
+              </NavLink>)}
+          </DrawerBody>
+        </DrawerContent>
+      </Drawer>
+        </ButtonGroup>
 
       </Flex>
       
       <Flex gap={16} mt={'16px'}>
-        <Box display={['none','block']} w={'250px'}>
-          <ul style={{position:'fixed'}}>
+        <Box display={['none','none','block']} w={'250px'}>
+          <ul style={{position:'fixed',fontSize:'20px'}}>
             {links.map(link=> 
-            <NavLink   className={`link ${(isActive)=>isActive ? "active" : null}`}  
-            to={link.to}><li style={{listStyleType:'none',fontWeight:'bold',width:'250px'}}>{link.name}</li></NavLink>)}
+            <NavLink 
+            className={`link ${(isActive)=>isActive ? "active" : null}`}  
+            to={link.to}>
+              <li style={{listStyleType:'none',fontWeight:'bold',width:'250px'}}>{link.name}</li>
+              </NavLink>)}
           </ul>
         </Box>
         <Box flex='1'>
