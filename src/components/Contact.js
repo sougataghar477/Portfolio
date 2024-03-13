0c424ffe7a36827b0b58cf899448ebb5f1ca1f5e
sougata <sougataghar47@gmail.com>
2024-03-13 23:04:25 +0530

    added images

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,7 +1,7 @@
 import { Heading,Link,Text,Box,Image } from "@chakra-ui/react"
 function Contact(){
     return <>
-        <Image src="https://images.pexels.com/photos/1829198/pexels-photo-1829198.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"/>
+        <Image src="/contact.webp"/>
         <Box mt={8} lineHeight={1.6}>
         <Heading>Email 📬</Heading>
         <Text fontSize={20}><Link href='mailto:sougataghar47@gmail.com' isExternal> sougataghar47@gmail.com</Link></Text>     
