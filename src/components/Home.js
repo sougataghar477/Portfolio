08110dfec9c4477a3960f31ec29ec7c993054188
sougata <sougataghar47@gmail.com>
2024-03-28 12:03:26 +0530

    added new project

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,7 +5,7 @@ function Home(){
     <Heading textAlign={'center'}>Welcome to my website!</Heading>
     <Image mt={4} src='/hello.jpeg'/>
     <Text mt={4} fontSize={20} mx={'auto'} lineHeight={1.6} fontWeight={'bold'} fontStyle={'italic'}>
-        My name is Sougata Ghar and I am a front end web developer who is proficient in HTML, CSS, Javascript,React,Vue,Next JS,Nuxt JS,Express JS and MongoDB. 
+        My name is Sougata Ghar and I am a front end web developer who is proficient in HTML, CSS, Javascript,React,Vue,Next JS,Express JS and MongoDB. 
     </Text>
     
     <Link to={'/projects'}><Button w={'100%'} mt={4}>Check out my projects ➡️</Button></Link>
