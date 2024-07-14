57911c75578bd6ed87fcadc0a3bd7a4fc8a0228c
sougata <sougataghar47@gmail.com>
2024-07-14 18:14:31 +0530

    fixed header

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,12 +5,13 @@ import MobileNavigationBar from "./MobileNavigationBar";
 function Header(){
     const { isOpen, onOpen, onClose } = useDisclosure();
     const { colorMode, toggleColorMode } = useColorMode();
-    return <Flex justifyContent={'space-between'} alignItems={'center'} position={'fixed'} w={'90ch'} zIndex={10}>
+    return <Flex gap={16} justifyContent={['flex-start','space-between','space-between']} alignItems={'center'} position={['static','fixed','fixed']} w={'90ch'} zIndex={10}>
     <Heading>Sougata Ghar</Heading>
     <ButtonGroup>
     <Button 
     onClick={toggleColorMode} 
-    mt={4}  
+    mt={4}
+    mr={4}  
      >{colorMode==='light'?<SunIcon/>:<MoonIcon/>}
     </Button>
     <Button display={['flex','flex','none']} onClick={onOpen} mt={4} ><HamburgerIcon/></Button>
